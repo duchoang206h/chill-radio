88b309fbf3507b9f24b2fba17d81fbd3f62d9014
duchoang <duchoang206h@gmail.com>
2021-10-17 16:07:48 +0700

    update acording to restful api rules

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,26 @@ const router = express.Router();
 const apicontroller = require('../controllers/apicontroller')
 const Main = require('../controllers/getlistvideo');
 let g = new Main();
-router.post('/search', apicontroller.search);
-router.get('/getCurrentVideo', (req,res)=>{
+// Route /api/v1/search
+router.get('/search', apicontroller.search.get);
+router.post('/search',apicontroller.search.post);
+router.put('/search',apicontroller.search.put );
+router.delete('/search',apicontroller.search.delete );
+// Route /api/v1/video
+router.get('/video', (req,res)=>{
     const currentVideo =  g.getCurrentVideo();
     console.log(currentVideo);
     res.status(200).json(currentVideo);
   
 });
+router.post('/video', (req,res)=>{
+  res.status(200).json({msg:"Chua code xong"})
+});
+router.put('/video', (req,res)=>{
+    res.status(200).json({msg:"Chua code xong"})
+  });
+router.delete('/video', (req,res)=>{
+    res.status(200).json({msg:"Chua code xong"})
+  });
 module.exports = router;
+
