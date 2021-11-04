04fe52ca4926c13efa316b66e55c510ff14871ec
Duc Hoang <duchoang206h@gmail.com>
2021-11-04 23:49:33 +0700

    update oauth using jwt

--- public/outh0.js
+++ public/outh0.js
@@ -1,66 +1,7 @@
-let auth0 = null;
-const uri = origin + "/config";
-const uri_encode = encodeURI(uri);
-const configureClient = async () => {
-  const config = await axios.get(uri_encode);
-  auth0 = await createAuth0Client(config.data);
-};
-let loadd = async () => {
-  await configureClient();
-  console.log("load xong ");
-  updateUI();
 
-  const isAuthenticated = await auth0.isAuthenticated();
-
-  if (isAuthenticated) {
-    // show the gated content
-    return;
-  }
-
-  // NEW - check for the code and state parameters
-  const query = window.location.search;
-  if (query.includes("code=") && query.includes("state=")) {
-    // Process the login state
-    try {
-      await auth0.handleRedirectCallback();
-    } catch (error) {
-      console.log(error);
-    }
-
-    updateUI();
-
-    // Use replaceState to redirect the user away and remove the querystring parameters
-    window.history.replaceState({}, document.title, "/");
-  }
-};
-loadd();
-const login = async () => {
-  await auth0.loginWithRedirect({
-    redirect_uri: window.location.origin,
-  });
-};
-
-const logout = async () => {
-  auth0.logout({
-    returnTo: window.location.origin,
-  });
-};
-
-const updateUI = async () => {
-  const isAuthenticated = await auth0.isAuthenticated();
-  if (isAuthenticated) {
-    const user = await auth0.getUser();
-    document.getElementById("userImg").src = user.picture;
-    document.getElementById("userName").innerText += user.name;
-    document.getElementById("user").style.display = "flex";
-  } else {
-   /*  document.getElementById("userImg").src = "images/default_avata.png"; */
-  }
-};
 async function addtoplaylist() {
   // check author
-  const isAuthenticated = await auth0.isAuthenticated();
-  if (isAuthenticated) {
+  if (decodeURIComponent(document.cookie)) {
     $("#search").css("display", "block");
   } else {
     console.log("not login");
