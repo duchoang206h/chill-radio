e5f7eee0a8475cceeaf9c2944f8aecaab86eeff9
Duc Hoang <duchoang206h@gmail.com>
2021-11-04 17:38:19 +0700

    update for cleaner

--- helpers/queue.js
+++ helpers/queue.js
@@ -1,26 +1,33 @@
 class Queue {
   constructor() {
     this.data = [];
-  }
-  size() {
-    return this.data.length;
-  }
-  isEmpty() {
-    return this.data.length === 0;
+    this.priodata = [];
   }
   enqueue(value) {
-    this.data.push(value);
-    return true;
-  }
-  dequeue() {
-    if (this.isEmpty()) return undefined;
-    return this.data.shift();
+    this.priodata.push(value);
   }
   addarray(arr) {
     this.data = [...this.data, ...arr];
   }
-  getAllData(){              // Return the whole data for playlist on client
-    return this.data;
+  dequeue() {
+    if (this.priodata.length == 0) {
+      /* if (this.data.length == 0) return undefined; */
+         this.data.shift();
+      return this.data.slice(0,1)[0]
+    } else {
+      this.priodata.shift();
+      return this.priodata.slice(0,1)[0]
+    }
+  }
+  size() {
+    return this.data.length + this.priodata.length;
+  }
+  isEmpty() {
+    return this.size() == 0;
+  }
+  getAllData() {
+    // Return the whole data for playlist on client
+    return [...this.priodata, ...this.data];
   }
 }
 module.exports = Queue;
