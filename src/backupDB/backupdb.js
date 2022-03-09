e7b784752f95de125a5a5fe538480500457ab1dd
Duc Hoang <duchoang206h@gmail.com>
2022-03-09 20:39:13 +0700

    update backup data

--- src/backupDB/backupdb.js
+++ src/backupDB/backupdb.js
@@ -5,12 +5,16 @@ const { CronJob } = require('cron');
 const mongose = require('mongoose');
 const connectBK = mongose.createConnection('mongodb+srv://duchoang206h:120202@cluster0.zhldv.mongodb.net/chillradio?retryWrites=true&w=majority', {useNewUrlParser: true,useUnifiedTopology: true});
 const VideoBackupModel = connectBK.model('Video',VideoSchema);
-const previousVideoCount = 0;
 const backup = async () =>{
     try {
-        const result = await videoService.findByFiller({skip:previousVideoCount})
-        const previousVideoCount = result.length;
-        await VideoBackupModel.insertMany(result);
+        const result = await videoService.findAll();
+        for(let i =0;i<result.length;i++){
+           const response = await VideoBackupModel.findOne({videoId:result[i].videoId});
+           if(!response) VideoBackupModel.create(result[i]);
+           else{
+               VideoBackupModel.updateOne({videoId:result[i]},{like:result[i].like,dislike:result[i].dislike})
+           }
+        }
     } catch (error) { 
         console.log(error);
     }
